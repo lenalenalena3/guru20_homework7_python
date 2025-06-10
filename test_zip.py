5cd07c22abdd489f3f381138827e387df878abf0
Elena Tinkalyuk <lenalenalena3@mail.ru>
2025-06-10 22:20:07 +0300

    доработка констант

--- test_zip.py
+++ test_zip.py
@@ -5,11 +5,10 @@ import csv
 from io import TextIOWrapper
 from openpyxl import load_workbook
 
-NAME_ARCHIVE = 'zip_file.zip'
 BASE_DIR = pathlib.Path('.')
 PATH_FILES_DOWNLOAD = BASE_DIR / 'tmp'
 PATH_ARCHIVE = BASE_DIR / 'attachment'
-PATH_NAME_ZIP = PATH_ARCHIVE / NAME_ARCHIVE
+PATH_NAME_ZIP = PATH_ARCHIVE / 'zip_file.zip'
 NAME_FILE_PDF = 'file_example_PDF.pdf'
 NAME_FILE_CSV = 'file_example_CSV.csv'
 NAME_FILE_XLSX = 'file_example_XLSX.xlsx'
